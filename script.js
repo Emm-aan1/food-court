084bed5d3867b10be15faf76e7b8ed67f9a5bdfd
Chidoka Ifeanyi <114946207+Emm-aan1@users.noreply.github.com>
2023-04-16 08:33:24 +0100

    initial

---
+++ script.js
@@ -0,0 +1,18 @@
+const burger = document.getElementById('burger');
+const menu = document.getElementById('menu');
+const close = document.getElementById('close');
+const open = document.getElementById('open');
+
+// event listener
+burger.addEventListener('click', () => {
+  if (menu.classList.contains('hidden')) {
+    menu.classList.remove('hidden');
+    close.classList.remove('hidden');
+    open.classList.add('hidden');
+
+  } else {
+    menu.classList.add('hidden');
+    close.classList.add('hidden');
+    open.classList.remove('hidden');
+  }
+})
